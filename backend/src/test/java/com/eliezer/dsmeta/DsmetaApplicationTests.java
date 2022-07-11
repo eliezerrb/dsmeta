3e2c1de56082a10010cb9ca434c5da6c4a758bc7
eliezerrb <eliezerrezende@outlook.com>
2022-07-11 20:41:55 -0300

    Project created

---
+++ backend/src/test/java/com/eliezer/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliezer.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
